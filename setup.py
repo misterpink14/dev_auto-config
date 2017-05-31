66bed5532cb25fda2f928eec5fe8bc7b8037a658
misterpink14 <benjamint@qualtrics.com>
2017-05-31 10:27:32 -0700

    Added pretty basic install for nvim

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
 #/usr/local/bin/python3
 '''
 TODO:
-    [] neovim should be installed if needed
     [] add comments
     [] clean up a bit
     [] additional plugin requirements
+    [] need to add pip install for neovim
+pip2 install --user neovim
+pip3 install --user neovim
 '''
 
 import os
+import subprocess
+
+def has_neovim():
+    """Checks to see if nvim is installed
+    """
+    return subprocess.call(["which", "nvim"]) == 0
+
+def install_neovim():
+    """Installs neovim
+    """
+    subprocess.call(["brew", "install", "neovim/neovim/neovim"])
 
 def setup_neovim():
+    """Copys init.vim to nvim config folder
+    """
     user_home = os.path.expanduser("~")
     neovim_init_out = user_home + "/.config/nvim/init.vim"
     neovim_init_in = "./init.vim"
@@ -17,12 +32,27 @@ def setup_neovim():
     with open(neovim_init_in, "r") as fi:
         with open(neovim_init_out, "w") as fo:
             fo.write(fi.read())
-    
+
+def install_vimplug():
+    """Installs vim-plug
+    """
+    subprocess.call(["curl",
+                     "-fLo",
+                     "~/.local/share/nvim/site/autoload/plug.vim",
+                     "--create-dirs",
+                     "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"])
+
+def main():
+    """Updates nvim config file, installs nvim if it hasn't been yet
+    """
+    if not has_neovim():
+        print("Installing NeoVim")
+        install_neovim()
+    install_vimplug()
+    print("Setting up NeoVim")
+    setup_neovim()
+    print("dun")
 
 
-
-setup_neovim()
-print("dun")
-
-
-
+if __name__ == "__main__":
+    main()
