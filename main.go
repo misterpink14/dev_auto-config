eb58d81c671360f974e4705cd8847135a4596789
BJ Thompson <benjamint@qualtrics.com>
2017-10-20 16:20:53 -0700

    Added BrewDependency and ModBrewDependency, switched from fmt to log

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
+	"os/exec"
 )
 
 // All dependencies should implement this function
 type BaseDependency interface {
 	Install()
 	Update()
+	checkInstall()
 	CheckInstall()
 }
 
@@ -19,61 +21,98 @@ type Dependency struct {
 	update  string // command()
 }
 
+type BrewDependency struct {
+	Dependency
+	command string
+}
+
+type ModBrewDependency struct {
+	BrewDependency
+	bashCommand string
+}
+
 // Installs a dependency
 func (d *Dependency) Install() {
-	fmt.Println(d.install)
+	log.Println(d.install)
 }
 
 // Updates a dependency
 func (d *Dependency) Update() {
-	fmt.Println(d.update)
+	log.Println(d.update)
+}
+
+func (d *Dependency) checkInstall(cmd string) {
+	_, err := exec.Command("which", cmd).Output()
+	if err != nil {
+		if err.Error() == "exit status 1" {
+			return false
+		}
+		log.Fatalln("Failed to execute which command for: ", d.command, err)
+	}
+	return true
 }
 
 // Checks if a dependency needs to be installed
-func (d *Dependency) CheckInstall() {
-	fmt.Println("which " + d.command)
-}
-
-//func NewDependency (brewCommand string) d Dependency {
-//	d := Dependency{
-//		command: brewCommand,
-//		install: "brew install " + command,
-//		install: "brew update " + command
-//	}
-//
-//	return d
-//}
-
-type BrewDependency struct {
-	*Dependency
-	command string
+func (d *Dependency) CheckInstall() bool {
+	d.checkInstall(d.command)
 }
 
 func (d *BrewDependency) Install() {
-	fmt.Println("brew install " + d.command)
+	log.Println("brew install " + d.command)
 }
 
 func (d *BrewDependency) Update() {
-	fmt.Println("brew update " + d.command)
-	fmt.
+	log.Println("brew update " + d.command)
 }
 
-//type NeoVim struct {
-//}
+func (d *ModBrewDependency) CheckInstall() {
+	d.checkInstall(d.bashCommand)
+}
+
+func handleDependency(d Dependency) {
+	if d.CheckInstall() {
+		d.Update()
+	} else {
+		d.Install()
+	}
+}
+
+func HandleDependency(dCommand, dInstall, dUpdate string) {
+	d := Dependency{
+		command: dCommand,
+		install: dInstall,
+		update:  dUpdate}
+	handleDependency(d)
+}
+
+func HandleBrewDependency(bCommand) {
+	bd := BrewDependency{command: bCommand}
+	handleDependency(bd)
+}
+
+func HandleModBrewDependency() {
+	mbd := ModBrewDependency{command: brewCommand, bashCommand: bashCommand}
+	handleDependency(mbd)
+}
 
 func main() {
 	hasBrew := flag.Bool("brew", false, "Install or update brew")
 	hasNeovim := flag.Bool("nvim", false, "Install or update vim")
-	hasInitvim := flag.Bool("initnvim", false, "Copy init.vim")
+	hasInitvim := flag.Bool("init.vim", false, "Copy init.vim")
 
 	flag.Parse()
 
-	test := BrewDependency{command: "nvim"}
-	test.CheckInstall()
-	test.Install()
-	test.Update()
+	log.Println(*hasBrew)
+	log.Println(*hasNeovim)
+	log.Println(*hasInitvim)
 
-	fmt.Println(*hasBrew)
-	fmt.Println(*hasNeovim)
-	fmt.Println(*hasInitvim)
+	if *hasBrew {
+		HandleDependency(
+			"brew",
+			"/usr/bin/ruby -e $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)",
+			"brew upgrade && brew upgrade")
+	}
+	if *hasNeovim() {
+		HandleNeovimDependency()
+	}
 }
